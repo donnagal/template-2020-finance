2c228076f38e79793f3b1b3f05a4c7b4e8dbe89e
Donna Galletta <donna.galletta@isobar.com>
2020-01-17 16:11:20 +1100

    update template

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-AOS.init();
 
-$(".button-m").click(function(){
-    $(".menu").slideToggle("hidden");
-  });
+var swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  mousewheel: {
+    releaseOnEdges: true,
+  },
+  hashNavigation: true,
+  on: {
+    slideChange: function() {
+     $('.count').each(function () {
+          $(this).prop('Counter',0).animate({
+              Counter: $(this).text()
+          }, {
+              duration: 500,
+              easing: 'swing',
+              step: function (now) {
+                  $(this).text(Math.ceil(now));
+              }
+          });
+      });
+    }
+  }
+});
